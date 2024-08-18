d85713321c87e73601cd39fc808928955e1456f2
Thibault <thibault.borde@etu.emse.fr>
2024-08-18 13:33:45 +0200

    made Header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import '../styles/Header.css';
-import OlympicLogo from '../assets/Olympic_Logo.png';
 
 function Header() {
   return (
     <header className="header">
       <div className="container-fluid">
-        <div class="row">
-        <div class="col-md-2"></div>
-          <div class="col-xs-4">
-
-            <h1 className="header-title fs-1 text-center">Olympic Games Medal Visualization</h1>
-
-            <img src={OlympicLogo} className="img-responsive" alt="Olympic flag" />
+        <div className="row">
+          <div className="col-12">
+            <h1 className="header-title text-center ">Olympic Games Medal Visualization</h1>
           </div>
-          <div class="col-md-2"></div>
         </div>
-
       </div>
     </header>
   );
