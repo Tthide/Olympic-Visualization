df0fabb393d440c448c136c29404a2207111e057
Thibault <thibault.borde@etu.emse.fr>
2024-08-19 13:46:48 +0200

    removed d3 import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './fonts/Roboto-Bold.ttf';
-import * as d3 from "d3";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
