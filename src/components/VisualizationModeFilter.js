53125428561ca9717ce59473c9581d898c7ee1ea
Thibault <thibault.borde@etu.emse.fr>
2024-08-19 16:27:05 +0200

    fixed method bound error

--- src/components/VisualizationModeFilter.js
+++ src/components/VisualizationModeFilter.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const VisualizationModeFilter = () => {
+const VisualizationModeFilter = ({ handleChange }) => {
     return (
         <>
             <label for="dropdown-VisualizationMode">Choose a Visualization type:</label>
-            <select name="dropdown" id="dropdown-VisualizationMode" onChange={(value) => this.props.handleChange('VisualizationMode', value)} >
+            <select name="dropdown" id="dropdown-VisualizationMode" onChange={(value) => handleChange('VisualizationMode', value)} >
                 <option value="PieChart" >Pie chart</option>
                 <option value="Treemap">Treemap</option>
                 <option value="Choropleth">Choropleth</option>
