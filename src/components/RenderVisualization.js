d23c286a6107aa3355757a224778050d40bf6794
Thibault <thibault.borde@etu.emse.fr>
2024-08-23 14:40:14 +0200

    display message when filters result in no data

--- src/components/RenderVisualization.js
+++ src/components/RenderVisualization.js
@@ -14,21 +14,29 @@ const RenderVisualization = ({ VisualizationMode, currentFilters }) => {
     if (VisualizationMode !== "None") {
 
 
-        const Data = FilterData({ dataset: summer, currentFilters , dictionary: dictionary});
-        //console.log(Data);
-        switch (VisualizationMode) {
-            case 'PieChart':
-                return <PieChart data={Data} />;
-            case 'Treemap':
-                return <Treemap />;
-            case 'Choropleth':
-                return <Choropleth />;
-            // Add more cases for other visualization components
-            default:
-                return <h2>Please select a visualization mode</h2>;
+        const Data = FilterData({ dataset: summer, currentFilters, dictionary: dictionary });
+
+
+        if (Object.keys(Data).length == 0) {
+            return <>
+                    <h2>No medals have been found</h2>
+                    <h2>Please change your filters</h2>
+                </>;
+        } else {
+            switch (VisualizationMode) {
+                case 'PieChart':
+                    return <PieChart data={Data} />;
+                case 'Treemap':
+                    return <Treemap />;
+                case 'Choropleth':
+                    return <Choropleth />;
+                // Add more cases for other visualization components
+                default:
+                    return <h2>Please select a visualization mode</h2>;
+            }
         }
 
-    }else{
+    } else {
 
         return <h2>Please select a visualization mode</h2>;
 
